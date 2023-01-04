3bc334e91e408c3f4c9bb02e382d4d18e54122ee
thulanimbatha <thulanimbatha06@gmail.com>
2023-01-04 13:25:31 +0200

    version 3.0 - update button enabled, user able to change movie rating and review

--- main.py
+++ main.py
@@ -13,6 +13,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
 Bootstrap(app)
 db = SQLAlchemy(app=app)
 
+# WTForm - RateMovieForm
+class RateMovieForm(FlaskForm):
+    rating_form = StringField(label='Your rating out of 10 (e.g. 7.5)', validators=[DataRequired()])
+    review_form = StringField(label='Your review', validators=[DataRequired()])
+    submit = SubmitField(label='Done')
+
 # create Movies table
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,24 +33,44 @@ class Movie(db.Model):
 app.app_context().push()
 db.create_all()
 
+# new_movie = Movie(title="Phone Booth",
+#                         year=2002,
+#                         description="Publicist Stuart Shepard finds himself trapped in a phone booth, "\
+#                             "pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, "\
+#                                 "Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#                         rating=7.3,            
+#                         ranking=10,
+#                         review="My favourite character was the caller.",
+#                         img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+#                     )
+# db.session.add(new_movie)
+# db.session.commit()
+
 @app.route("/")
 def home():
-    new_movie = Movie(title="Phone Booth",
-                        year=2002,
-                        description="Publicist Stuart Shepard finds himself trapped in a phone booth, "\
-                            "pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, "\
-                                "Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-                        rating=7.3,            
-                        ranking=10,
-                        review="My favourite character was the caller.",
-                        img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-                    )
-                        
-    return render_template("index.html", movie=new_movie)
+    # get all movies
+    all_movies = Movie.query.all()          
+    return render_template("index.html", movies=all_movies)
 
 @app.route('/add')
 def add():
     return render_template('add.html')
+
+@app.route('/edit', methods=['GET', 'POST'])
+def rate_movie():
+    rate_movie_form = RateMovieForm()
+    # get current movie id
+    movie_id = request.args.get('id')
+    # use that id to get the movie associated with it
+    movie = Movie.query.get(movie_id)
+    if rate_movie_form.validate_on_submit():
+        # change the rating and the review
+        movie.rating = float(rate_movie_form.rating_form.data)
+        movie.review = rate_movie_form.review_form.data
+        # commit changes onto database
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template('edit.html', form=rate_movie_form, movie=movie)
 
 
 if __name__ == '__main__':
